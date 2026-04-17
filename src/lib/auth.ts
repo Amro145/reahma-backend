b855f0447783e7ea0e9c501ad9b5c25aa6557162
Amro145 <amroaltayeb14@gmail.com>
2026-04-18 00:18:39 +0200

    GOOD

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -14,9 +14,5 @@ export const initAuth = (env: any) => betterAuth({
             clientId: env.GOOGLE_CLIENT_ID as string,
             clientSecret: env.GOOGLE_CLIENT_SECRET as string,
         },
-        github: {
-            clientId: env.GITHUB_CLIENT_ID as string,
-            clientSecret: env.GITHUB_CLIENT_SECRET as string,
-        },
     },
 });
