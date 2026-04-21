2d0c46446bdb98d7564d81d446c961bae358269d
Amro145 <amroaltayeb14@gmail.com>
2026-04-21 18:25:24 +0200

    feat: recreate students table with updated schema and foreign key constraints

---
+++ drizzle/0004_overrated_legion.sql
@@ -0,0 +1,17 @@
+PRAGMA foreign_keys=OFF;--> statement-breakpoint
+CREATE TABLE `__new_students` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`organizationId` text NOT NULL,
+	`name` text NOT NULL,
+	`whatsapp` text,
+	`requiredAmount` real NOT NULL,
+	`status` text DEFAULT 'pending' NOT NULL,
+	`enrollmentDate` integer DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	`createdAt` integer NOT NULL,
+	FOREIGN KEY (`organizationId`) REFERENCES `organization`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+INSERT INTO `__new_students`("id", "organizationId", "name", "whatsapp", "requiredAmount", "status", "enrollmentDate", "createdAt") SELECT "id", "organizationId", "name", "whatsapp", "requiredAmount", "status", "enrollmentDate", "createdAt" FROM `students`;--> statement-breakpoint
+DROP TABLE `students`;--> statement-breakpoint
+ALTER TABLE `__new_students` RENAME TO `students`;--> statement-breakpoint
+PRAGMA foreign_keys=ON;
