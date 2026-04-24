b77489bc4b71d6357ced55459730e112c506fe1f
Amro145 <amroaltayeb14@gmail.com>
2026-04-25 00:14:29 +0300

    chore: update drizzle database schema snapshot and journal

--- drizzle/0001_silly_golden_guardian.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE `user` ADD COLUMN `password` text NOT NULL;
-ALTER TABLE `students` ADD COLUMN `faculty` text NOT NULL;
-ALTER TABLE `students` ADD COLUMN `semester` text NOT NULL;
-ALTER TABLE `students` ADD COLUMN `enrollmentDate` integer DEFAULT CURRENT_TIMESTAMP NOT NULL;
-ALTER TABLE `students` ALTER COLUMN `whatsapp` DROP NOT NULL;
-ALTER TABLE `students` ALTER COLUMN `status` SET DEFAULT 'pending';
